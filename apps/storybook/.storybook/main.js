5981f45917ac3d4a6ac7a50ba87c58adbe593dff
Tomche Jovanovski <tomche@oneprogram.com>
2026-05-16 20:12:02 +0200

    fix storybook dependencies

--- apps/storybook/.storybook/main.js
+++ apps/storybook/.storybook/main.js
@@ -1,12 +1,19 @@
 module.exports = {
-  stories: ['../../packages/ui/src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../../../packages/ui/src/**/*.stories.@(js|jsx|ts|tsx)'],
+
   addons: [
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
     '@storybook/addon-links',
+    '@storybook/addon-a11y',
   ],
+
   framework: {
-    name: '@storybook/react',
+    name: '@storybook/react-vite',
     options: {},
+  },
+
+  docs: {
+    autodocs: 'tag',
   },
 };
